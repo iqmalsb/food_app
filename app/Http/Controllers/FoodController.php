1527edeb5a457ca7c3ea3b08e165fc7f5ab2b980
Iqmal Shamsul Bahari <iqmalshamsul96@gmail.com>
2022-06-11 12:14:34 +0800

    add create/store function FoodController

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Food;
 use Illuminate\Http\Request;
 
 class FoodController extends Controller
 {
     public function index() {
-        return view ('food.index');
+        $foods = Food::all();
+
+        return view ('food.index', compact('foods'));
+    }
+
+    public function create() { 
+        return view ('food.create');
+    }
+
+    public function store(Request $request) {
+        Food::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'image' => $request->image,
+        ]);
+        return to_route('food.index');
     }
 }
