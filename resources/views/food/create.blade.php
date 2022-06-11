2997c11ca91f574f9070db6e10bda59c890e681c
Iqmal Shamsul Bahari <iqmalshamsul96@gmail.com>
2022-06-11 17:02:12 +0800

    update create blade food

--- resources/views/food/create.blade.php
+++ resources/views/food/create.blade.php
@@ -6,6 +6,16 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Food Create New</div>
+                
+                  @if ($errors->any())
+                    <div class="alert alert-danger">
+                      <ul>
+                          @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                          @endforeach
+                      </ul>
+                    </div>
+                  @endif
 
                 <div class="card-body">
                     <form  method="POST" action="{{ route('food.store') }}" enctype="multipart/form-data">
