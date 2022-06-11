c3d50a57f6d0d5209cf8adf6c45a80014690402b
Iqmal Shamsul Bahari <iqmalshamsul96@gmail.com>
2022-06-11 12:13:50 +0800

    update index blade food

--- resources/views/food/index.blade.php
+++ resources/views/food/index.blade.php
@@ -1,1 +1,40 @@
-Index food
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Food Index</div>
+
+                <div class="card-body">
+                    <a href="{{ route('food.create') }}"type="button" class="btn btn-dark">Add New Food</a>
+                    
+                </div>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Image</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($foods as $food)
+                                <tr>
+                                    <th scope="row">{{$loop->iteration}}</th>
+                                    <td>{{$food->name}}</td>
+                                    <td>{{$food->description}}</td>
+                                    <td>{{$food->image}}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                      </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
