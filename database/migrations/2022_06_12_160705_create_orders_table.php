907d45c301f0ab971be3a6b58454bea8b2be7308
Iqmal Shamsul Bahari <iqmalshamsul96@gmail.com>
2022-06-13 00:15:33 +0800

    Migration Create Orders

---
+++ database/migrations/2022_06_12_160705_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('total_price');
+            $table->boolean('is_paid')->default(false);
+            $table->boolean('is_dine_in')->default(true);
+            $table->boolean('has_cutlery')->default(false);
+            $table->string('additional_request');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
