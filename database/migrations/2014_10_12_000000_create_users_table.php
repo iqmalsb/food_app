1d4f99e974c43f878347fd53eedc1cc035e8828c
Iqmal Shamsul Bahari <iqmalshamsul96@gmail.com>
2022-06-12 23:45:34 +0800

    Migration Update users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,16 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image')->nullable();
+            $table->string('role')->default('staff');
+            $table->string('contact_no')->default('');
+            $table->string('position')->default('');
+            $table->string('address_line1')->default('');
+            $table->string('address_line2')->default('');
+            $table->string('address_line3')->default('');
+            $table->string('address_city')->default('');
+            $table->string('address_state')->default('');
+            $table->string('address_postcode')->default('');
             $table->rememberToken();
             $table->timestamps();
         });
