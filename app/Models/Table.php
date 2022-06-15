cc7b6cdc7204d423646a405d357903c6cc88bcaa
Iqmal Shamsul Bahari <iqmalshamsul96@gmail.com>
2022-06-16 02:29:09 +0800

    Update table name

--- app/Models/Table.php
+++ app/Models/Table.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Table extends Model
 {
     use HasFactory;
-    protected $table = 'reservations';
+    protected $table = 'tables';
 
     protected $fillable = [
         'table_no',
