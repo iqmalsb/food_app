182d4c1c7ede3a7044d5652fe6a39bf16503e2c5
Iqmal Shamsul Bahari <iqmalshamsul96@gmail.com>
2022-06-20 20:41:40 +0800

    Rename food route

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,11 @@ Auth::routes(['verify' => true]);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // Food Routes
-Route::get('/foods', [App\Http\Controllers\FoodController::class, 'index'])->name('food.index');
-Route::get('/foods/create', [App\Http\Controllers\FoodController::class, 'create'])->name('food.create');
-Route::post('/foods/store', [App\Http\Controllers\FoodController::class, 'store'])->name('food.store');
-Route::get('/foods/{food}', [App\Http\Controllers\FoodController::class, 'show'])->name('food.show');
-Route::post('/foods/{food}/update', [App\Http\Controllers\FoodController::class, 'update'])->name('food.update');
-Route::get('/foods/{food}/delete', [App\Http\Controllers\FoodController::class, 'delete'])->name('food.delete');
+Route::get('/food', [App\Http\Controllers\FoodController::class, 'index'])->name('food.index');
+Route::get('/food/create', [App\Http\Controllers\FoodController::class, 'create'])->name('food.create');
+Route::post('/food/store', [App\Http\Controllers\FoodController::class, 'store'])->name('food.store');
+Route::get('/food/{food}', [App\Http\Controllers\FoodController::class, 'show'])->name('food.show');
+Route::post('/food/{food}/update', [App\Http\Controllers\FoodController::class, 'update'])->name('food.update');
+Route::get('/food/{food}/delete', [App\Http\Controllers\FoodController::class, 'delete'])->name('food.delete');
 
 
