42d8bd7369b15e90e766dfd9b0135bf2f800fe61
Iqmal Shamsul Bahari <iqmalshamsul96@gmail.com>
2022-06-24 00:36:14 +0800

    Add Categories Routes

--- routes/web.php
+++ routes/web.php
@@ -29,4 +29,10 @@ Route::get('/food/{food}', [App\Http\Controllers\FoodController::class, 'show'])
 Route::post('/food/{food}/update', [App\Http\Controllers\FoodController::class, 'update'])->name('food.update');
 Route::get('/food/{food}/delete', [App\Http\Controllers\FoodController::class, 'delete'])->name('food.delete');
 
-
+// Category Routes
+Route::get('/categories', [App\Http\Controllers\CategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/create', [App\Http\Controllers\CategoryController::class, 'create'])->name('categories.create');
+Route::post('/categories/store', [App\Http\Controllers\CategoryController::class, 'store'])->name('categories.store');
+Route::get('/categories/{category}', [App\Http\Controllers\CategoryController::class, 'show'])->name('categories.show');
+Route::post('/categories/{category}/update', [App\Http\Controllers\CategoryController::class, 'update'])->name('categories.update');
+Route::get('/categories/{category}/delete', [App\Http\Controllers\CategoryController::class, 'delete'])->name('categories.delete');
